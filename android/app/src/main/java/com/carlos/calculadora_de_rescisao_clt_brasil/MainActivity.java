ff667a3518b2a4f58d6727e67a3449960ff6b2e4
Carlos Fernando <carlospopo2@gmail.com>
2026-02-26 05:44:11 -0300

    Update 26/02/2026  5:44:11,38

--- android/app/src/main/java/com/carlos/calculadora_de_rescisao_clt_brasil/MainActivity.java
+++ android/app/src/main/java/com/carlos/calculadora_de_rescisao_clt_brasil/MainActivity.java
@@ -1,5 +1,17 @@
 package com.carlos.calculadora_de_rescisao_clt_brasil;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inicializa AdMob
+        MobileAds.initialize(this, initializationStatus -> {
+        });
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,65 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import {
+  AdMob,
+  BannerAdSize,
+  BannerAdPosition
+} from '@capacitor-community/admob'
+
+
+async function startAdMob(){
+
+  try{
+
+    // inicializa
+    await AdMob.initialize({
+      requestTrackingAuthorization: false
+    })
+
+    console.log("AdMob inicializado")
+
+    // banner
+    await AdMob.showBanner({
+      adId: 'ca-app-pub-3940256099942544/6300978111',
+      adSize: BannerAdSize.ADAPTIVE_BANNER,
+      position: BannerAdPosition.BOTTOM_CENTER,
+      isTesting: true
+    })
+
+    console.log("Banner exibido")
+
+    // interstitial
+    await AdMob.prepareInterstitial({
+      adId: 'ca-app-pub-3940256099942544/1033173712',
+      isTesting: true
+    })
+
+    setTimeout(async () => {
+
+      await AdMob.showInterstitial()
+      console.log("Interstitial exibido")
+
+    }, 15000)
+
+  }
+  catch(e){
+
+    console.log("Erro AdMob:", e)
+
+  }
+
+}
+
+
+// iniciar quando app carregar
+startAdMob()
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
